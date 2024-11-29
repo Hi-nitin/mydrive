1a6105fed52b94df81c5eeda5739807195d3c24d
Hi-nitin <nmnitin5@gmail.com>
2024-11-29 04:16:12 -0800

    k

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App=()=>{
 
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}/>
+        <Route path="/" element={<Home />}/>
 <Route path="/login" element={<Login/>}/>
           <Route path="/signup" element={<Signup />}/>
           <Route path="/dashboard" element={<Dashboard />}/>
